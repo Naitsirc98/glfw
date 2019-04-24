908a0715a28953ae7ba7bb231318f253e781c36c
Cristian Herrera <naitsirc98@gmail.com>
2019-04-24 01:35:39 +0100

    Modified premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,10 @@
+-- Only works for windows (for now)
 project "GLFW"
     kind "StaticLib"
     language "C"
     
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-inter/" .. outputdir .. "/%{prj.name}")
+	targetdir("bin/" .. output_dir .. "/%{prj.name}")
+    objdir("bin-inter/" .. output_dir .. "/%{prj.name}")
 
 	files
 	{
@@ -17,10 +18,10 @@ project "GLFW"
         "src/vulkan.c",
         "src/window.c"
     }
-    
+
 	filter "system:windows"
         buildoptions { "-std=c11", "-lgdi32" }
-        systemversion "10.0.17134.0"
+        systemversion "latest"
         staticruntime "On"
         
         files
@@ -40,6 +41,13 @@ project "GLFW"
 		{ 
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
-		}
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+        }
+
+        
+    filter
+     { 
+         "system:windows",
+          "configurations:Release"
+     }
+
+    buildoptions "/MT"
